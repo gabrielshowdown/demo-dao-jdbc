03cd6ea565e0f8fcd62d703ef132fdd47f595348
Gabriel Henrique <gabriell.hhenrique@hotmail.com.br>
2022-07-21 20:34:45 -0300

    Criando a tela de Update

--- src/application/Program.java
+++ src/application/Program.java
@@ -37,6 +37,12 @@ public class Program {
 		Seller newSeller = new Seller (null, "Greg", "Greg@gmail.com", new Date(), 4000.00, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted! New Id = " + newSeller.getId());
+		
+		System.out.println("\n=== TEST 5: Seller Update ===");
+		seller = sellerDao.findById(1); // Carregando os dados do vendedor no obj seller
+		seller.setName("Martha Waine");
+		sellerDao.update(seller);
+		System.out.println("Update Completed");
 	}
 
 }
